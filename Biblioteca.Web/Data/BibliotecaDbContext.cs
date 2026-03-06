e3cd6b14ca1881c33670034853afc2b6115ff9b7
Marcos Felipe França <m.felipefrc@hotmail.com>
2026-03-06 15:38:12 -0300

    WIP: start web + db setup

---
+++ Biblioteca.Web/Data/BibliotecaDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Biblioteca.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteca.Web.Data
+{
+    public class BibliotecaDbContext : DbContext
+    {
+        public BibliotecaDbContext(DbContextOptions<BibliotecaDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Livro> Livros => Set<Livro>();
+        public DbSet<Usuario> Usuarios => Set<Usuario>();
+        public DbSet<Emprestimo> Emprestimos => Set<Emprestimo>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Relacionamentos principais
+            modelBuilder.Entity<Emprestimo>()
+                .HasOne(e => e.Livro)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Emprestimo>()
+                .HasOne(e => e.Usuario)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
